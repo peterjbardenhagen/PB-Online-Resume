a7dedc6e60257e25b793e0543429cfc457e9a39f
peterjbardenhagen <90166296+peterjbardenhagen@users.noreply.github.com>
2024-11-15 11:03:07 +1000

    Fix

--- app/layout.js
+++ app/layout.js
@@ -12,16 +12,15 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <Head>
-
-                <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=2" />
                 <title>Peter Bardenhagen Online Resume & Chatbot</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
-                <link rel="shortcut icon" href="favicon.ico">
+                <link rel="shortcut icon" href="favicon.ico" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Peter Bardenhagen Online Resume & Chatbot" />
@@ -37,11 +36,9 @@ export default function RootLayout({ children }) {
                 <meta name="description" content="I'm an innovative digital enterprise leader with a proven track record collaborating with top-tier global consulting firms. Leveraging technology, I address business challenges, enhance customer experiences, and drive growth. My passion lies in building products, solving challenging business problems with AI powered solutions, and designing robust architectures." />
                 <meta property="og:locale" content="en_UK" />
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-H090T9HXWP"></script>
-                {/* <script src="Global.js" language="javascript"></script> */}
+                <script src="global.js" language="javascript"></script>
                 <link rel="canonical" href="https://peter.bardenhagen.xyz/" />
                 <link rel="alternate" hreflang="en-us" href="/" />
-
-
             </Head>
             <body>{children}</body>
         </html>
